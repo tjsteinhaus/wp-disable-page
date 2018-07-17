db243fdf12435c9a214903d3137d0d1769ad083a
Tyler Steinhaus <tyler.steinhaus@constellationmutual.com>
2018-07-17 11:04:55 -0500

    Data now saves when updated or published.

--- src/admin/CreateMetaBox.php
+++ src/admin/CreateMetaBox.php
@@ -20,6 +20,16 @@ class CreateMetaBox {
     const PRIORITY = 'core';
 
     /**
+     * WP Nonce Action
+     */
+    const NONCE_ACTION = 'WP_Disable_Page_Save';
+
+    /**
+     * WP Nonce Name
+     */
+    const NONCE_NAME = 'WP_Disable_Page';
+
+    /**
      * Runs all the fancy actions and filters to create our meta box.
      * 
      * @since 07/17/2018
@@ -27,6 +37,7 @@ class CreateMetaBox {
      */
     public function init() {
         add_action( 'add_meta_boxes', array( __CLASS__, 'createMetaBox' ) );
+        add_actioN( 'save_post', array( __CLASS__, 'saveMetaData' ) );
     }
 
     /**
@@ -46,7 +57,40 @@ class CreateMetaBox {
         );
     }
 
-    public function createView() {
+    /**
+     * Display's our meta box
+     * 
+     * @param $post \WP_Post
+     * 
+     * @since 07/17/2018
+     * @author Tyler Steinhaus
+     */
+    public function createView( \WP_Post $post ) {
         require( WPDisablePage_DIR . '/src/templates/admin/meta_box.phtml' );
     }
-}
+
+    /**
+     * Save the post meta data from our plugin
+     * 
+     * @param $post_id int
+     * 
+     * @since 07/17/2018
+     * @author Tyler Steinhaus
+     */
+    public function saveMetaData( $post_id ) {
+        // Check to see if multiple items should flag so the data isn't saved
+        if( 
+            !wp_verify_nonce( $_POST[self::NONCE_NAME], self::NONCE_ACTION )
+        ) {
+            return;
+        }
+
+        // Set the data we want to be saved
+        $is_disabled = ( isset( $_POST['wp_disable_page__is_disabled'] ) && $_POST['wp_disable_page__is_disabled'] != '' ) ? $_POST['wp_disable_page__is_disabled'] : '0';
+        $url = ( isset( $_POST['wp_disable_page__url'] ) && $_POST['wp_disable_page__url'] != '' ) ? $_POST['wp_disable_page__url'] : '';
+
+        // Save the data
+        update_post_meta( $post_id, 'wp_disable_page__is_disabled', $is_disabled );
+        update_post_meta( $post_id, 'wp_disable_page__url', $url );
+    }
+} 
